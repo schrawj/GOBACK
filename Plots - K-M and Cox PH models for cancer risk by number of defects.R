53b4dd56aba8fc6145c56bba4c8878aa92afb50f
Jeremy Schraw <schraw@bcm.edu>
2018-08-09 11:55:00 -0500

    New curves for new cancer variables
    Additional code to generate survival curves for new cancer groupings per Sharon's suggestion.

--- Plots - K-M and Cox PH models for cancer risk by number of defects.R
+++ Plots - K-M and Cox PH models for cancer risk by number of defects.R
@@ -158,3 +158,131 @@ print(ggsurvplot(fit,
                        linetype = 'strata', 
                        legend.labs = c('No defect', '1 defect', '2 defects', '3 defects', '4 or more defects')))
 
+
+
+# Generate Cox models for new variables -----------------------------------
+
+#'-------------------------------------------------------------------------
+#'-------------------------------------------------------------------------
+#' 2018.07.17.
+#' 
+#' Sharon suggested we use any cancer, any hematologic cancer, any cns
+#' tumor and any non-cns solid tumor as the categories.
+#' 
+#' Parameterize as categorical variable: 0 vs. 1, 2, 3, 4 or more defects.
+#'-------------------------------------------------------------------------
+#'-------------------------------------------------------------------------
+
+require(survival)
+
+load('Z:/Jeremy/GOBACK/Datasets/goback.nochrom.v20180711.rdata')
+
+goback.nochrom$majordefect.cat <- factor(ifelse(goback.nochrom$majordefect.total == 0, 0,
+                                                ifelse(goback.nochrom$majordefect.total == 1, 1,
+                                                       ifelse(goback.nochrom$majordefect.total == 2, 2, 
+                                                              ifelse(goback.nochrom$majordefect.total == 3, 3,
+                                                                     ifelse(goback.nochrom$majordefect.total >= 4, 4, NA))))),
+                                         levels = c(0:4),
+                                         labels = c('0', '1', '2', '3', '4 or more'))
+
+
+#' Need new variables for any heme cancer (any leukemia or any lymphoma) + 
+#' any non-CNS solid tumor.
+goback.nochrom$any.heme.cancer <- ifelse(goback.nochrom$leu.any == 1 | goback.nochrom$lym.any == 1, 1, 0)
+
+solid.tumors <- unique(goback.nochrom$cancer1)
+solid.tumors <- subset(solid.tumors, !(solid.tumors %in% c(NA, 'all','leu.other','aml','hl','nhl','cns.other','medullo','pnet','lym.other',
+                                                           'gct.intra','astro','ependymoma')))
+
+goback.nochrom$any.non.cns.solid.tumor <- ifelse(goback.nochrom$cancer1 %in% solid.tumors, 1, 0)
+
+outcomes <- c('any.heme.cancer','any.non.cns.solid.tumor')
+
+#' Cox PH models for cancers of interest by number of birth defects.
+for (i in outcomes){
+  
+  goback.surv <- data.frame(studyid = goback.nochrom$studyid,
+                            time = goback.nochrom$person.yrs, 
+                            cancer = goback.nochrom[,i], 
+                            defect = goback.nochrom$majordefect.cat,
+                            sex = factor(goback.nochrom$sex, 
+                                         levels = c(1,2),
+                                         labels = c('Male','Female')),
+                            m.age = goback.nochrom$m.age,
+                            state = goback.nochrom$state)
+  
+  cox <- cox <- coxph(Surv(time, cancer) ~ defect + m.age + sex + state, data = goback.surv)
+  cox.coef <- as.data.frame(summary(cox)$coefficients)
+  
+  estimates <- data.frame(var = rownames(cox.coef), 
+                          hr = cox.coef$`exp(coef)`, 
+                          ci.lower = exp(cox.coef$coef-(1.96*cox.coef$`se(coef)`)), 
+                          ci.upper = exp(cox.coef$coef+(1.96*cox.coef$`se(coef)`)))
+  
+  write.csv(estimates, file = paste0('Z:/Jeremy/GOBACK/R outputs/Cancer risk by number of birth defects/',i,'.risk.by.num.defects.csv'), row.names = FALSE)
+  
+}
+
+
+
+# Generate K-M curves for new variables -----------------------------------
+
+require(survival); require(survminer)
+
+load('Z:/Jeremy/GOBACK/Datasets/goback.nochrom.v20180711.rdata')
+
+goback.nochrom$majordefect.cat <- factor(ifelse(goback.nochrom$majordefect.total == 0, 0,
+                                                ifelse(goback.nochrom$majordefect.total == 1, 1,
+                                                       ifelse(goback.nochrom$majordefect.total == 2, 2, 
+                                                              ifelse(goback.nochrom$majordefect.total == 3, 3,
+                                                                     ifelse(goback.nochrom$majordefect.total >= 4, 4, NA))))),
+                                         levels = c(0:4),
+                                         labels = c('0', '1', '2', '3', '4 or more'))
+
+
+#' Need new variables for any heme cancer (any leukemia or any lymphoma) + 
+#' any non-CNS solid tumor.
+goback.nochrom$any.heme.cancer <- ifelse(goback.nochrom$leu.any == 1 | goback.nochrom$lym.any == 1, 1, 0)
+
+solid.tumors <- unique(goback.nochrom$cancer1)
+solid.tumors <- subset(solid.tumors, !(solid.tumors %in% c(NA, 'all','leu.other','aml','hl','nhl','cns.other','medullo','pnet','lym.other',
+                                                           'gct.intra','astro','ependymoma')))
+
+goback.nochrom$any.non.cns.solid.tumor <- ifelse(goback.nochrom$cancer1 %in% solid.tumors, 1, 0)
+
+outcomes <- c('any.heme.cancer','cns.any','any.non.cns.solid.tumor')
+
+the.plots.thicken <- list()
+
+for (i in 1:length(outcomes)){
+  
+  goback.surv <- data.frame(studyid = goback.nochrom$studyid,
+                            time = goback.nochrom$person.yrs, 
+                            cancer = goback.nochrom[,outcomes[i]], 
+                            defect = goback.nochrom$majordefect.cat,
+                            sex = factor(goback.nochrom$sex, 
+                                         levels = c(1,2),
+                                         labels = c('Male','Female')),
+                            m.age = goback.nochrom$m.age,
+                            state = goback.nochrom$state)
+  
+  fit <- survfit(Surv(time, cancer) ~ defect, data = goback.surv)
+  
+  new.plot <- ggsurvplot(fit, 
+                         conf.int = FALSE, 
+                         ylab = NULL,
+                         ylim = c(0.99, 1), 
+                         xlab = NULL,
+                         xlim = c(0,18), 
+                         font.tickslab = c(15, 'bold', 'black'),
+                         linetype = 'strata', 
+                         legend = "none")
+  
+  the.plots.thicken[i] <- new.plot
+  
+}
+
+names(the.plots.thicken) <- outcomes
+
+
+
