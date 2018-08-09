4a6f2fc80a2c2c312dbd3d83cc46940e0cd905f1
Jeremy Schraw <schraw@bcm.edu>
2018-08-09 11:54:16 -0500

    New significance categories
    Revised heatmaps to a 3 color scheme, and removed deprecated code to make script as short as possible.

--- Plots - heatmaps production.R
+++ Plots - heatmaps production.R
@@ -1,127 +1,23 @@
 #'-------------------------------------------------------------------------
 #'-------------------------------------------------------------------------
-#' GOBACK heatmaps, production version.
+#' The minimal production version of the code for generating GOBACK 
+#' heatmaps.
 #'-------------------------------------------------------------------------
 #'-------------------------------------------------------------------------
 
+require(dplyr); require(ggplot2)
 
-
-# Prep environment --------------------------------------------------------
-require(dplyr); require(stringr); require(ggplot2); require(ggthemes)
-
-load('Z:/Jeremy/GOBACK/Datasets/Expanded datasets/goback.cox.ph.results.v20180124.1.rdata')
-
-
-
-# Generate heatmap --------------------------------------------------------
-
-require(dplyr)
-
-load('Z:/Jeremy/GOBACK/Datasets/Expanded datasets/goback.cox.ph.results.v20180124.1.rdata')
+load('Z:/Jeremy/GOBACK/Datasets/Expanded datasets/goback.coxph.results.v20180612.rdata')
 
 #' Need an updated defects matrix.
+#' Omit osteosarcoma from cancers, because no associations were tested.
 defects <- c("conganomalies.cns", "conganomalies.eye", "conganomalies.ear.face.neck", "conganomalies.heart.circsys",    
              "conganomalies.respsys", "oral.clefts", "conganomalies.digestivesystem", "conganomalies.genitalandurinary",
-             "conganomalies.musculoskelsys", "conganomalies.integument", "chromosomalanomalies")
-cancers <- c('all','aml','hl','nhl','cns.any','astro','medullo','neuro','retino','nephro','hepato','bone.any','osteo','rms.any','soft.other','gct.any')
-
-bd.cc.associations <- data.frame(defect = rep(defects, each = 16), cancer = rep(cancers, 11))
-bd.cc.associations <- left_join(bd.cc.associations, goback.coxmodels, by = c('defect','cancer'))
-bd.cc.associations <- rename(bd.cc.associations, hr = HR, ci.lower = CI.lower, ci.upper = CI.upper)
-
-bd.cc.associations$signif.cat <- 9999
-bd.cc.associations$signif.cat <- factor(
-  ifelse(bd.cc.associations$signif.cat == 9999 & is.na(bd.cc.associations$hr), 6,
-         ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper > 1, 2,
-                ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 1 & bd.cc.associations$hr <= 5, 3,
-                       ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 5 & bd.cc.associations$hr <= 25, 4,
-                              ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 25, 5,
-                                     ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper < 1 & bd.cc.associations$hr < 1, 1, NA)))))),
-  levels = c(1:6),
-  labels = c('< 1','Null','1-5', '5.01-25','>25','Not Tested'))
-
-#' Convert defect and cancer variables to factor form for prettier display.
-bd.cc.associations$cancer <- factor(bd.cc.associations$cancer, 
-                                    levels = c('all','aml','hl','nhl','cns.any','astro','medullo', 'retino',
-                                               'neuro','nephro','hepato','bone.any','osteo','rms.any','soft.other','gct.any'),
-                                    labels = c('ALL', 'AML', "Hodgkin's Lymphoma", "Non-Hodgkin's Lymphoma", 'Any CNS Tumor', 'Astrocytoma','Medulloblastoma', 'Retinoblastoma',
-                                               'Neuroblastoma',"Wilm's Tumor",'Hepatoblastoma','Any Bone Tumor','Osteosarcoma','Rhabdomyosarcoma','Non-RMS Soft Tissue Sarcoma',
-                                               'Any Germ Cell Tumor'))
-
-bd.cc.associations$defect <- factor(bd.cc.associations$defect, 
-                                    levels = c( "conganomalies.cns",               "conganomalies.eye",               "conganomalies.ear.face.neck",     "conganomalies.heart.circsys",    
-                                                "conganomalies.respsys",           "oral.clefts",                     "conganomalies.digestivesystem",   "conganomalies.genitalandurinary",
-                                                "conganomalies.musculoskelsys",    "conganomalies.integument",        "chromosomalanomalies"),
-                                    labels = c('Any CNS Anomaly', 'Any Eye Anomaly', 'Any Ear, Face or Neck Anomaly', 'Any Heart or Circulatory System Anomaly',
-                                               'Any Respiratory System Anomaly', 'Cleft Lip and Cleft Palate', 'Any Digestive System Anomaly', 'Any Genitourinary Anomaly',
-                                               'Any Musculoskeletal Anomaly', 'Any Integument Anomaly', 'Any Chromosomal Anomaly'))
-
-plot<-  ggplot(data = bd.cc.associations, aes(x = cancer, y = defect)) +
-  geom_tile(aes(fill = signif.cat), color = 'black') + 
-  scale_fill_manual(values = c('green','grey50','indianred1','firebrick1','darkred','white'), drop = FALSE) +
-  guides(fill = guide_legend(title='Hazard Ratio')) +
-  ggtitle('Figure 1. Associations Between Different Categories of Birth Defects and Childhood Cancers') +
-  labs(x = "Cancer", y = "Anomaly") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,hjust = 1))
-
-print(plot)
-
-
-
-# Omit osteosarcoma (no associations tested) ------------------------------
-
-defects <- c("conganomalies.cns", "conganomalies.eye", "conganomalies.ear.face.neck", "conganomalies.heart.circsys",    
-             "conganomalies.respsys", "oral.clefts", "conganomalies.digestivesystem", "conganomalies.genitalandurinary",
-             "conganomalies.musculoskelsys", "conganomalies.integument", "chromosomalanomalies")
+             "conganomalies.musculoskelsys", "conganomalies.integument", "any.chromosomal.anomaly")
 cancers <- c('all','aml','hl','nhl','cns.any','astro','medullo','retino','neuro','nephro','hepato','bone.any','rms.any','soft.other','gct.any')
 
 bd.cc.associations <- data.frame(defect = rep(defects, each = 15), cancer = rep(cancers, 11))
 bd.cc.associations <- left_join(bd.cc.associations, goback.coxmodels, by = c('defect','cancer'))
-bd.cc.associations <- rename(bd.cc.associations, hr = HR, ci.lower = CI.lower, ci.upper = CI.upper)
-
-bd.cc.associations$signif.cat <- 9999
-bd.cc.associations$signif.cat <- factor(
-  ifelse(bd.cc.associations$signif.cat == 9999 & is.na(bd.cc.associations$hr), 6,
-         ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper > 1, 2,
-                ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 1 & bd.cc.associations$hr <= 5, 3,
-                       ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 5 & bd.cc.associations$hr <= 25, 4,
-                              ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 25, 5,
-                                     ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper < 1 & bd.cc.associations$hr < 1, 1, NA)))))),
-  levels = c(1:6),
-  labels = c('< 1','Null','1-5', '5.01-25','>25','Not Tested'))
-
-#' A significance category variable with more gradations.
-bd.cc.associations$signif.cat.fine <- 9999
-bd.cc.associations$signif.cat.fine <- factor(
-  
-                                               ifelse(bd.cc.associations$signif.cat.fine == 9999 & is.na(bd.cc.associations$hr), 9,
-                                               ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper > 1, 2,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 1 & bd.cc.associations$hr <= 5, 3,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 5 & bd.cc.associations$hr <= 10, 4,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 10 & bd.cc.associations$hr <= 15, 5,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 15 & bd.cc.associations$hr <= 20, 6,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 20 & bd.cc.associations$hr <= 25, 7,
-                                               ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 25, 8,
-                                               ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper < 1, 1, NA))))))))),
-                                      
-                                      levels = c(1:9),
-                                      labels = c('< 1','Null','1.01-5', '5.01-10','10.01-15','15.01-20','20.01-25','>25','Not Tested'))
-
-#' Intermediate number of gradations.
-bd.cc.associations$signif.cat.int <- 9999
-bd.cc.associations$signif.cat.int <- factor(
-  
-ifelse(bd.cc.associations$signif.cat.int == 9999 & is.na(bd.cc.associations$hr), 7,
-ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper > 1, 2,
-ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 1 & bd.cc.associations$hr <= 5, 3,
-ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 5 & bd.cc.associations$hr <= 10, 4,
-ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 10 & bd.cc.associations$hr <= 20, 5,
-ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$ci.upper > 1 & bd.cc.associations$hr > 20, 6,
-ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper < 1, 1, NA))))))),
-  
-levels = c(1:7),
-labels = c('< 1','Null','1.01-5', '5.01-10','10.01-20','>20','Not Tested'))
 
 #' Convert axis variables to factor form for prettier display.
 bd.cc.associations$cancer <- factor(bd.cc.associations$cancer, 
@@ -134,22 +30,30 @@ bd.cc.associations$cancer <- factor(bd.cc.associations$cancer,
 bd.cc.associations$defect <- factor(bd.cc.associations$defect, 
                                     levels = c( "conganomalies.cns",               "conganomalies.eye",               "conganomalies.ear.face.neck",     "conganomalies.heart.circsys",    
                                                 "conganomalies.respsys",           "oral.clefts",                     "conganomalies.digestivesystem",   "conganomalies.genitalandurinary",
-                                                "conganomalies.musculoskelsys",    "conganomalies.integument",        "chromosomalanomalies"),
+                                                "conganomalies.musculoskelsys",    "conganomalies.integument",        "any.chromosomal.anomaly"),
                                     labels = c('Any CNS Anomaly', 'Any Eye Anomaly', 'Any Ear, Face or Neck Anomaly', 'Any Heart or Circulatory System Anomaly',
                                                'Any Respiratory System Anomaly', 'Oral Clefts', 'Any Digestive System Anomaly', 'Any Genitourinary Anomaly',
                                                'Any Musculoskeletal Anomaly', 'Any Integument Anomaly', 'Any Chromosomal Anomaly'))
 
-
-
-# Print poduction plot ----------------------------------------------------
+#' Bins associations into the currently agreed upon categories based on their HR.
+bd.cc.associations$signif.cat <- 9999
+bd.cc.associations$signif.cat <- factor(
+                                        ifelse(bd.cc.associations$signif.cat == 9999 & is.na(bd.cc.associations$hr), 6,
+                                        ifelse(bd.cc.associations$ci.lower < 1 & bd.cc.associations$ci.upper > 1, 2,
+                                        ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$hr > 1 & bd.cc.associations$hr <= 5, 3,
+                                        ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$hr > 5 & bd.cc.associations$hr <= 10, 4,
+                                        ifelse(bd.cc.associations$ci.lower > 1 & bd.cc.associations$hr > 10, 5,
+                                        ifelse(bd.cc.associations$ci.upper < 1 & bd.cc.associations$hr < 1, 1, NA)))))),
+  levels = c(1:6),
+  labels = c('< 1','Null','1-5', '5.01-10','>10','Not Tested'))
 
 #' Intermediate number of gradations, all red.
 #' Do not plot title, or force <1 category to display in legend.
-plot3 <-  ggplot(data = bd.cc.associations, aes(x = cancer, y = defect)) +
-  geom_tile(aes(fill = signif.cat.int), color = 'black') + 
-  scale_fill_manual(values = c('grey75','indianred1','firebrick3','red','darkred','white')) +
-  guides(fill = guide_legend(title='Hazard Ratio')) +
-  labs(x = "Cancer", y = "Anomaly") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
-print(plot3)
+p <-  ggplot(data = bd.cc.associations, aes(x = cancer, y = defect)) +
+             geom_tile(aes(fill = signif.cat), color = 'black') + 
+             scale_fill_manual(values = c('grey75','indianred1','firebrick3','darkred','white')) +
+             guides(fill = guide_legend(title='Hazard Ratio')) +
+             labs(x = "Cancer", y = "Anomaly") +
+             theme_bw() +
+             theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+print(p)
